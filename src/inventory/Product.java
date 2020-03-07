bdbe21a7a9eb3ee5191d3248c1546fc059c68e52
Traphume <traphumedev@gmail.com>
2020-03-08 01:05:11 +0700

    Added getter and setter for stock attribute to interface

--- src/inventory/Product.java
+++ src/inventory/Product.java
@@ -1,9 +1,23 @@
 package inventory;
 
 public interface Product {
-    String getName();
-
+    // for id attribute
     int getId();
 
+    void setId(int id);
+
+    // for name attribute
+    String getName();
+
+    void setName(String name);
+
+    // for price attribute
     double getPrice();
+
+    void setPrice(double price);
+
+    // for stock attribute
+    int getStock();
+
+    void setStock(int stock);
 }
