a2f103d1d3cb3a8882123368282c98c828851445
Traphume <traphumedev@gmail.com>
2020-03-08 15:26:40 +0700

    Added example

---
+++ src/example/BookInventoryDemo.java
@@ -0,0 +1,30 @@
+package example;
+
+import inventory.Product;
+import store.*;
+
+public class BookInventoryDemo {
+    public static void main(String[] args) {
+        BookInventory inventory = new BookInventory();
+
+        // add a new book
+        Book animalBook = new Book(1, "Animal Book", 30, 1);
+        inventory.create(animalBook);
+
+
+        BookInventoryMemento memento = inventory.save();
+
+        // add another book
+        Book insectBook = new Book(2, "Insect Book", 30, 1);
+        inventory.create(insectBook);
+
+        // load state from memento
+        inventory.restore(memento);
+
+        Product prod = inventory.find(2);
+        System.out.printf("Product is null because we loaded state from memento: %s\n", prod);
+
+        prod = inventory.find(1);
+        System.out.printf("Product 1 is Animal Book: %s\n", prod.getName());
+    }
+}
