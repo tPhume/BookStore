306380e1aee941b5b30067e6131031c3b950103c
Traphume <traphumedev@gmail.com>
2020-03-08 18:07:56 +0700

    Implemented BookCreateCommand

---
+++ src/store/BookCreateCommand.java
@@ -0,0 +1,17 @@
+package store;
+
+import command.InventoryCommand;
+import inventory.Inventory;
+
+public class BookCreateCommand implements InventoryCommand {
+    private Book book;
+
+    public BookCreateCommand(Book book) {
+        this.book = new Book(book.getId(), book.getName(), book.getPrice(), book.getStock());
+    }
+
+    @Override
+    public void execute(Inventory inventory) {
+        inventory.create(book);
+    }
+}
