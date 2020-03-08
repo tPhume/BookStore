329bead8b41e2f901035634d7d5edc645789a72a
Traphume <traphumedev@gmail.com>
2020-03-08 15:47:35 +0700

    Reformat file

--- src/store/BookInventoryMemento.java
+++ src/store/BookInventoryMemento.java
@@ -9,7 +9,7 @@ public class BookInventoryMemento implements Memento {
     private ArrayList<Book> bookList;
 
     BookInventoryMemento(ArrayList<Book> bookList) {
-        this.bookList = bookList;
+        this.bookList = new ArrayList<>(bookList);
     }
 
     @Override
