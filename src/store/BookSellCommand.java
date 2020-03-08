a5afcb6e246748f14df034b5f06be358cbd62c72
Traphume <traphumedev@gmail.com>
2020-03-08 18:17:40 +0700

    Implemented BookSellCommand

---
+++ src/store/BookSellCommand.java
@@ -0,0 +1,31 @@
+package store;
+
+import command.InventoryCommand;
+import inventory.Inventory;
+
+public class BookSellCommand implements InventoryCommand {
+    private int id;
+    private String name;
+    private int num;
+
+    public BookSellCommand(int id, int num) {
+        this.id = id;
+        this.num = num;
+        this.name = null;
+    }
+
+    public BookSellCommand(String name, int num) {
+        this.id = -1;
+        this.num = num;
+        this.name = name;
+    }
+
+    @Override
+    public void execute(Inventory inventory) {
+        if (this.id != -1) {
+            inventory.sell(this.id, this.num);
+        } else {
+            inventory.sell(this.name, this.num);
+        }
+    }
+}
