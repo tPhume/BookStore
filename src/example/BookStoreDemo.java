80b4fe77fb3dad8056ad332c92dcc353332d41c2
Traphume <traphumedev@gmail.com>
2020-03-09 11:02:10 +0700

    Updated example

--- src/example/BookStoreDemo.java
+++ src/example/BookStoreDemo.java
@@ -12,16 +12,25 @@ public class BookStoreDemo {
         BookInventory bookInventory = new BookInventory();
         BookDecorator bookDecorator = new BookDecorator(bookInventory, caretaker, "demo.command.ser");
 
-        // create new product
+        // create new product - Animal Book
         Book animalBook = new Book(1, "Animal Book", 500, 10);
         bookDecorator.create(animalBook);
+
+        // Sell 5 copies of Animal Book
+        bookDecorator.sell(1, 5);
 
         // create backup to file using caretaker and memento
         bookDecorator.save();
 
-        // update file
+        // update file - but don't make memento
         Book insectBook = new Book(2, "Insect Book", 300, 5);
         bookDecorator.create(insectBook);
+
+        System.out.println("---- Before restoration ----");
+        System.out.println(bookDecorator.find(1));
+        System.out.println(bookDecorator.find(2));
+
+        System.out.println();
 
         // now assume that our program crash
         // we will instantiate book inventory and co to new variables to demonstrate restore and replay ability
